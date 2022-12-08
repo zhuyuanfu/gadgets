0b0335df2f7ab52b8405ce9cd5f7ecab357f2104
Zhu Yuanfu <641561609@qq.com>
2022-12-08 17:52:27 +0800

    我也不知道改了什么，但是可能用不着了，开完会学校的命令可能会变

--- src/main/java/cn/edu/njfu/zyf/runnable/TextMessageSenderRunnable.java
+++ src/main/java/cn/edu/njfu/zyf/runnable/TextMessageSenderRunnable.java
@@ -18,7 +18,7 @@ public class TextMessageSenderRunnable implements Runnable{
     private Assigner assigner;
     private List<Dormitory> untestedDorms;
     
-    private String template = "齐心协力，共抗疫情。宿舍%s昨天没有人接受核酸检测，请被选中的你今天抽点时间去大学生活动中心接受一次核酸检测，为全宿舍挣一份安心。";
+    private String template = "齐心协力，共抗疫情。宿舍%s今天没有人接受核酸检测，请被选中的你明天抽时间去大学生活动中心接受一次核酸检测，为全宿舍挣一份安心。";
     
     public TextMessageSenderRunnable(TextMessageSender sender, 
             Assigner assigner,
